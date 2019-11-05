45b61ece34a554fe9a6e42948175aa6f76f670c5
NathanMarts <54146961+NathanMarts@users.noreply.github.com>
2019-11-05 16:35:49 -0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -1,5 +1,9 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "dependente.h"
 #include "empregado.h"
 
-int hash(int n, int l);
+void initHash(FILE *h, int tam);
+int hash(int n, int tam, int l);
+void inserirHash(FILE *h, FILE *r, FILE *exclusao, Empregado *emp, int tam, int l, int *qtd_registros);
+int expandHash(FILE *h, FILE *r, int p, int tam, int l);
