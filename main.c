e6a09d8ce9c3d30df20127bb3c6fa64903157b8c
BarbaraBLM <47249285+BarbaraBLM@users.noreply.github.com>
2019-11-06 17:56:34 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "hash.h"
 
 int main(){
-    FILE* emp = fopen("empregado.dat", w+b);
+    FILE* emp = fopen("empregado.dat", "w+b");
     Empregado* Joana = criarEmpregado("Joana", 18, 1650.0, 0);
     
     //Aki, teremos as funções de hash extensível
