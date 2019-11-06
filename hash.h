7602344eec02de0b35b5a7dc9cbbfa02e1035363
NathanMarts <54146961+NathanMarts@users.noreply.github.com>
2019-11-06 05:31:06 -0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -6,4 +6,4 @@
 void initHash(FILE *h, int tam);
 int hash(int n, int tam, int l);
 void inserirHash(FILE *h, FILE *r, FILE *exclusao, Empregado *emp, int tam, int l, int *qtd_registros);
-int expandHash(FILE *h, FILE *r, int p, int tam, int l);
+void expandHash(FILE *h, FILE *r, int p, int tam, int l);
