a3f6e47b07e0e0b98ff3f38b89db4ee0bc7f7c81
NathanMarts <54146961+NathanMarts@users.noreply.github.com>
2019-11-06 16:25:47 -0300

    Update empregado.c

--- empregado.c
+++ empregado.c
@@ -3,15 +3,16 @@
 #include <string.h>
 #include "empregado.h"
 
-Empregado* criarEmpregado(char* n, int idade, double sal, int n_dep){
-    int static cod = 0;
-    Empregado* e = malloc(tamanhoEmpregado());
+Empregado* criarEmpregado(char* n, int idade, double sal){
+    int static cod = 1;
+    Empregado* e = (Empregado *) malloc(sizeof(Empregado));
+    if (e) memset(e, 0, sizeof(Empregado));
     e->cod = cod;
     cod++;
     strcpy(e->nome, n);
     e->idade = idade;
     e->salario = sal;
-    e->n_dependentes = n_dep;
+    e->n_dependentes = 0;
     e->prox = -1;
     e->status = 1;
     return e;
@@ -21,11 +22,11 @@ void salva_empreg(Empregado *e, FILE *out){
     if(out != NULL){
         fwrite(&e->cod, sizeof(int), 1, out);
         fwrite(e->nome, sizeof(char), sizeof(e->nome), out);
-        fwrite(&e->idade, sizeof(int), sizeof(e->idade), out);
-        fwrite(&e->salario, sizeof(double), sizeof(e->salario), out);
-        fwrite(&e->n_dependentes, sizeof(int), sizeof(e->n_dependentes), out);
-        fwrite(&e->prox, sizeof(int), sizeof(e->prox), out);
-        fwrite(&e->status, sizeof(int), sizeof(e->status), out);
+        fwrite(&e->idade, sizeof(int), 1, out);
+        fwrite(&e->salario, sizeof(double), 1, out);
+        fwrite(&e->n_dependentes, sizeof(int), 1, out);
+        fwrite(&e->prox, sizeof(int), 1, out);
+        fwrite(&e->status, sizeof(int), 1, out);
     }
     else    printf("File passado é null");
     
@@ -40,18 +41,24 @@ Empregado* le_empreg(FILE *in){
         return NULL;
     }
     fread(e->nome, sizeof(char), sizeof(e->nome), in);
-    fread(&e->idade, sizeof(int), sizeof(e->idade), in);
-    fread(&e->salario, sizeof(double), sizeof(e->salario), in);
-    fread(&e->n_dependentes, sizeof(int), sizeof(e->n_dependentes), in);
-    fread(&e->prox, sizeof(int), sizeof(e->prox), in);
-    fread(&e->status, sizeof(int), sizeof(e->status), in);
+    fread(&e->idade, sizeof(int), 1, in);
+    fread(&e->salario, sizeof(double), 1, in);
+    fread(&e->n_dependentes, sizeof(int), 1, in);
+    fread(&e->prox, sizeof(int), 1, in);
+    fread(&e->status, sizeof(int), 1, in);
     return e;
 }
 
 void imprime_empreg(Empregado *e){
-    printf("\n\nEmpregado:\nCod: %d\nNome: %s\nIdade:%d\nSalario:%f\nNº dependentes:%d\nProx:%d\nStatus:%d\n", (e->cod), e->nome, e->idade, e->salario, e->n_dependentes, e->prox, e->status);
+    printf("\n\nEmpregado:\nCod: %d\nNome: %s\nIdade:%d\nSalario:%f\nNº dependentes:%d\nProx:%d\nStatus:%d\n", e->cod, e->nome, e->idade, e->salario, e->n_dependentes, e->prox, e->status);
 }
 
 int tamanhoEmpregado(){
-    return (sizeof(int) + 50*sizeof(char) + sizeof(int) + sizeof(double) + sizeof(int));
+    return sizeof(int)      //cod
+        + sizeof(char) * 50 //nome
+        + sizeof(int)       //idade
+        + sizeof(double)    //salario
+        + sizeof(int)       //n_dependentes
+        + sizeof(int)       //status
+        + sizeof(int);      //prox
 }
