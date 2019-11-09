8943875f3fd85eefc9f19a37e40e82657e50310a
NathanMarts <54146961+NathanMarts@users.noreply.github.com>
2019-11-09 19:13:51 -0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -7,3 +7,4 @@ void initHash(FILE *h, int tam);
 int hash(int n, int tam, int l);
 void inserirHash(FILE *h, FILE *r, FILE *exclusao, Empregado *emp, int tam, int l, int *qtd_registros);
 void expandHash(FILE *h, FILE *r, int p, int tam, int l);
+int busca_por_cod(FILE *hash, FILE* regts, int cod, int tam, int l);
